d3de5557155a71f445986addd15009365eb526c9
Ramcharan Panchakarla <114348732+Ramcharan71@users.noreply.github.com>
2025-11-27 22:12:26 +0530

    env

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      "*"
+      "https://frontend-machine-monitoring-dashboa.vercel.app"
     ],
     credentials: true,
   });
